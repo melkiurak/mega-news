7052ca0025ac6b5ecea482898e526a705f375679
melkiurak <melkiurak3209@gmail.com>
2025-05-20 12:12:12 +0300

    Header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,18 +1,32 @@
+import { FaSearch } from "react-icons/fa";
+import { GoKebabHorizontal } from "react-icons/go";
+
 export const Header = () => {
     return <header>
-        <div>
-            <div>
+        <div className="container flex justify-between">
+            <div className="flex-1">
                 <div>
-                    <h2 className="text-[22px] text-[#FC4308]">MEGA.news</h2>
+                    <h2 className="text-[22px] text-[#FC4308] font-roboto font-bold">MEGA.news</h2>
                 </div>
                 <nav>
-                    <ul>
+                    <ul className="flex gap-[34px] ">
                         <li><a href="#">Categories</a></li>
                         <li><a href="#">Pages</a></li>
                         <li><a href="#">Contact Us</a></li>
                         <li><a href="#">About Us</a></li>
                     </ul>
                 </nav>
+            </div>
+            <div className="flex-1">
+                <div>
+                    <button><GoKebabHorizontal/></button>
+                    <input type="text" />
+                    <button><FaSearch/></button>
+                </div>
+                <div>
+                    <button>Log In</button>
+                    <button>Sign Up </button>
+                </div>
             </div>
         </div>
     </header>
