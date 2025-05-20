6a030cf7cbe20b011121565b70b5ae6d3cbdd9be
melkiurak <melkiurak3209@gmail.com>
2025-05-20 11:45:33 +0300

    Header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,19 @@
+export const Header = () => {
+    return <header>
+        <div>
+            <div>
+                <div>
+                    <h2 className="text-[22px] text-[#FC4308]">MEGA.news</h2>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="#">Categories</a></li>
+                        <li><a href="#">Pages</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">About Us</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+}
