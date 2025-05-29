9657e885f0d4df0e1e18bda414ab981f05a1e11b
melkiurak <melkiurak3209@gmail.com>
2025-05-29 20:27:28 +0300

    styles for phone

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,21 +3,32 @@ import { GoKebabHorizontal } from "react-icons/go";
 
 export const Header = () => {
     return <header className="h-[48px] mt-[45px]">
-        <div className="container flex justify-between gap-2.5 h-full">
-           <div className="h-full">
-                <button className='w-12 h-12 relative z-[2] overflow-hidden bg-[#F5F5F5] rounded-2xl'>
+        <div className="container flex items-center justify-between flex-wrap gap-2.5 h-full">
+            <div className="h-full flex items-center gap-8">
+                <button className='lg:hidden w-12 h-12 relative z-[2] overflow-hidden bg-[#F5F5F5] rounded-2xl'>
                     <span className=' w-5 h-[2px] absolute top-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#3E3232] transition-all duration-500 rounded-2xl'></span>
                     <span className=' w-5 h-[2px] absolute top-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#3E3232] transition-all duration-500 rounded-2xl' style={{ top: "calc(50% - 5px)" }}></span>
                     <span className=' w-5 h-[2px] absolute top-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#3E3232] transition-all duration-500 rounded-2xl' style={{ top: "calc(50% + 5px)" }}></span>
                 </button>
-           </div>
-           <div className="flex-1">
-                <div className="relative h-full text-[#3E3232]">
+                <h4 className="font-roboto font-bold text-[22px] text-[#FC4308] max-phone:hidden">Mega.news</h4>
+                <nav className="max-lg:hidden">
+                    <ul className="flex items-center gap-[34px]">
+                        <li><a href="">Categories</a></li>
+                        <li><a href="">Pages</a></li>
+                        <li><a href="">contact us</a></li>
+                        <li><a href="">about us</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="flex items-center gap-[50px] max-phone:hidden">
+                    <div><span>userName</span></div>
+                    <div><span>save</span></div>
+            </div>
+            <div className="max-phone:flex-1 relative h-full text-[#3E3232] w-full">
                     <button className="absolute  rotate-90 top-3.5 left-1 text-xl"><GoKebabHorizontal/></button>
                     <input className="bg-[#F5F5F5] rounded-2xl h-full w-full pl-9 inputText" type="text" placeholder="Search anything" />
                     <button className="absolute right-4 top-3.5 text-xl"><FaSearch/></button>
-                </div>
-           </div>
+            </div>
         </div>
     </header>
 }
