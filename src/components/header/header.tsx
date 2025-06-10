b5964ee43e4e44c3c8b11e9391c7eabc89cdf432
melkiurak <melkiurak3209@gmail.com>
2025-06-10 20:01:30 +0300

    end header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,19 +4,27 @@ import { GoKebabHorizontal } from "react-icons/go";
 export const Header = () => {
     return <header className="h-[48px] mt-[45px]">
         <div className="container flex items-center justify-between phone:flex-wrap gap-2.5 h-full">
-            <div className="h-full flex items-center gap-8">
-                <button className='lg:hidden w-12 h-12 relative z-[2] overflow-hidden bg-[#F5F5F5] rounded-2xl'>
+            <div className="h-full flex items-center gap-8 lg:flex-1">
+                <button className='xl:hidden w-12 h-12 relative z-[2] overflow-hidden bg-[#F5F5F5] rounded-2xl'>
                     <span className=' w-5 h-[2px] absolute top-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#3E3232] transition-all duration-500 rounded-2xl'></span>
                     <span className=' w-5 h-[2px] absolute top-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#3E3232] transition-all duration-500 rounded-2xl' style={{ top: "calc(50% - 5px)" }}></span>
                     <span className=' w-5 h-[2px] absolute top-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#3E3232] transition-all duration-500 rounded-2xl' style={{ top: "calc(50% + 5px)" }}></span>
                 </button>
                 <h4 className="font-roboto font-bold text-[22px] text-[#FC4308] max-phone:hidden">Mega.news</h4>
-                <nav className="max-lg:hidden">
+                <nav className="max-xl:hidden">
                     <ul className="flex items-center gap-[34px]">
-                        <li><a href="">Categories</a></li>
-                        <li><a href="">Pages</a></li>
-                        <li><a href="">contact us</a></li>
-                        <li><a href="">about us</a></li>
+                        <li >
+                            <button>
+                                <span className="text-h5">Categories</span>
+                            </button>
+                        </li>
+                        <li>
+                            <button>
+                                <span className="text-h5">Pages</span>
+                            </button>
+                        </li>
+                        <li><a href="" className="text-h5">contact us</a></li>
+                        <li><a href="" className="text-h5">about us</a></li>
                     </ul>
                 </nav>
             </div>
@@ -24,7 +32,7 @@ export const Header = () => {
                 <div><span>userName</span></div>
                 <div><span>save</span></div>
             </div>
-            <div className="relative h-full text-[#3E3232] w-full lg:w-[300px] lg:order-1">
+            <div className="relative h-full text-[#3E3232] w-full lg:w-[465px] lg:order-1">
                 <button className="absolute  rotate-90 top-3.5 left-1 text-xl"><GoKebabHorizontal/></button>
                 <input className="bg-[#F5F5F5] rounded-2xl h-full w-full pl-9 inputText" type="text" placeholder="Search anything" />
                 <button className="absolute right-4 top-3.5 text-xl"><FaSearch/></button>
