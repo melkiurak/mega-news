5ae45e29f432163b439418a85887724cfaaaf7c7
melkiurak <melkiurak3209@gmail.com>
2025-06-14 19:28:55 +0300

    end one compontnen

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -3,30 +3,27 @@ import { ButtonSwitch } from '../buttonSwitch/ButtonSwitch';
 import singleContent1 from '@img/SingleContent.png' 
 import singleContent2 from '@img/SingleContent2.png' 
 export const Slider = () => {
+    
     return <div className='flex justify-between gap-5 items-center h-[367px] lg:h-[452px]'>
-        <div className='w-full h-full hidden lg:flex items-end flex-1 bg-no-repeat rounded-xl bg-cover bg-center ' style={{backgroundImage: `url("${singleContent1}")`}}>
-            <div className='bg-[#FFFFFFBF] rounded-xl m-[10px] w-full'>
-                <div className='p-[16px] overflow-hidden'>
-                    <h4 className='text-h4'>Why I Stopped Using Multiple Monitor </h4>
-                    <p className="truncate">Ah, the joy of the open road—it’s a good feeling. But if you’re new to driving, you may feel a little nervous about getting behind the wheel. Don’t worry. While it’s true that accidents can happen to anybody, there are things you can do to drive safely and do your best to avoid them. </p>
-                </div>
+        <div className="bg-card" style={{backgroundImage: `url("${singleContent2}")`}}>
+            <div className="bg-card__panel">
+                <h4 className='text-h4'>How to Drive a Car Safely</h4>
+                <p className='line-clamp-2'>Ah, the joy of the open road—it’s a good feeling. But if you’re new to driving, you may feel a little nervous about getting behind the wheel. Don’t worry. While it’s true that accidents can happen to anybody, there are things you can do to drive safely and do your best to avoid them.</p>
             </div>
-        </div> 
-        <div className='w-full h-full hidden lg:flex flex-1 bg-no-repeat rounded-xl bg-cover bg-center' style={{backgroundImage: `url("${singleContent2}")`}}>
-             <div className='bg-[#FFFFFFBF] rounded-xl m-[10px] w-full'>
-                <div className='p-[16px]'>
-                    <h4 className='text-h4'>Why I Stopped Using Multiple Monitor </h4>
-                </div>
+        </div>
+        <div className="bg-card" style={{backgroundImage: `url("${singleContent1}")`}}>
+            <div className="bg-card__panel">
+                <h4 className='text-h4'>How to Make Dance Music</h4>
+                <p className='line-clamp-2'>Ah, the joy of the open road—it’s a good feeling. But if you’re new to driving, you may feel a little nervous about getting behind the wheel. Don’t worry. While it’s true that accidents can happen to anybody, there are things you can do to drive safely and do your best to avoid them. </p>
             </div>
         </div>
         <div className='sm:flex flex-2 items-end hidden w-full h-full bg-no-repeat rounded-xl bg-cover bg-center relative' style={{backgroundImage: `url("${sliderImg}")`}}>
             <div className='px-2.5 absolute top-1/2 left-1/2  w-full transform -translate-x-1/2 -translate-y-1/2'>
                 <ButtonSwitch/>
             </div>
-            <div className='bg-[#FFFFFFBF] rounded-xl m-[10px] w-full'>
-                <div className='p-[16px]'>
-                    <h4 className='text-h4'>Why I Stopped Using Multiple Monitor </h4>
-                </div>
+             <div className="bg-card__panel">
+                <h4 className='text-h4'>Why I Stopped Using Multiple Monitor</h4>
+                <p className='hidden lg:line-clamp-2'>A Single Monitor Manifesto — Many developers believe multiple monitors improve productivity. Studies have proven it, right? Well, keep in mind, many of those studies are commissioned from monitor manufacturers like</p>
             </div>
         </div>
     </div>
