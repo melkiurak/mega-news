57d8ec87a38405092b46a43a676557a5438f7552
melkiurak <melkiurak3209@gmail.com>
2025-06-27 12:34:29 +0300

    Трачу время зря

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,7 @@
 import { IoMdHome } from "react-icons/io";
-import { FaBookmark } from "react-icons/fa";
 import { FaUser } from "react-icons/fa6";
-import { MdOutlineKeyboardDoubleArrowUp } from "react-icons/md";
-
+import { MdOutlineKeyboardDoubleArrowUp, MdEmail } from "react-icons/md";
+import { FaInstagram, FaBookmark, FaTwitter   } from "react-icons/fa";
 export default function Footer() {
     return (
         <footer>
@@ -21,8 +20,8 @@ export default function Footer() {
                     <MdOutlineKeyboardDoubleArrowUp className="footer__icon"/>
                 </button>
             </div>
-            <div className="bg-[#F5F5F5] hidden phone:flex">
-                <div className="">
+            <div className="bg-[#F5F5F5] hidden phone:flex flex-wrap justify-between max-w-[696px] w-full  rounded-r-3xl">
+                <div className="max-w-[360px] w-full">
                     <div className="flex items-center pb-5">
                         <h4 className="text-h4">Mega News</h4>
                     </div>
@@ -41,12 +40,25 @@ export default function Footer() {
                         <li><a href="" className="text-[#3E3232] font-roboto text-xs">Technology</a></li>
                     </ul>
                 </div>
-                <div>
+                <div className="max-w-[368px] w-full">
                     <div>
-                        <h4>Newsletters</h4>
+                        <h4 className="text-h4">Newsletters</h4>
                     </div>
-                    <div>
-                        <input type='email' />
+                    <div className="relative w-full h-12">
+                        <input type='email' className="w-full h-full rounded-xl bg-white" />
+                        <MdEmail className="absolute right-2 top-1/2 -translate-y-1/2 text-[#3E3232] text-xl"/>
+                    </div>
+                </div>
+                <div>
+                    <h4 className="text-h4">Social Network</h4>
+                    <div className="h-10 flex  items-center gap-4">
+                        <button className="h-full flex items-center gap-2 px-4 text-white bg-[#F45C9F] bg-gradient-to-l from-[#F45C9F] to-[#FF7563] rounded-xl ">
+                            <FaInstagram/>
+                            <span className="text-btn">Instagram</span>
+                        </button>
+                        <button className="h-full bg-[#2CA5E0] bg-gradient-to-bl from-[#2CA5E0s] to-[#67C9F5] text-white rounded-xl p-2">
+                            <FaTwitter className="text-2xl"/>
+                        </button>
                     </div>
                 </div>
             </div>
