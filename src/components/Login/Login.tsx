9a9cf3a891468c32697ff00b1215bf0b2bb27320
melkiurak <melkiurak3209@gmail.com>
2025-07-05 15:50:37 +0300

    Easy styles for login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import type { CreateUser } from "../../types"
 import  Parse  from '../../lib/parseClient';
+import { FaRegUser } from "react-icons/fa";
+import { RxLockClosed } from "react-icons/rx";
 export const Login = () => {
     const [formData, setFormData] = useState<CreateUser>({
         name: '',
@@ -20,13 +22,34 @@ export const Login = () => {
     const handelExit = async () => {
         return await Parse.User.logOut();
     }
-    return <div className="">
-        <h1>login</h1>
-        <form className="flex flex-col gap-2.5 max-w-[200px] w-full">
-            <input className="border-2 border-blue-700" type="text" value={formData.name} onChange={(e) => setFormData({...formData, name:e.target.value})}/>
-            <input className="border-2 border-blue-700" type="password" value={formData.password} onChange={(e) => setFormData({...formData, password:e.target.value})}/>
-            <button type="submit" onClick={handleSubmit}>Login</button>
-        </form>
-        <button onClick={handelExit}>Exit</button>
+    return <div className="flex items-center justify-center h-[100vh]">
+        <div className="flex flex-col text-center gap-5 border-[#E6E6E6] border-2 px-7 py-10 rounded-2xl max-w-[500px] w-full">
+            <h1>login</h1>
+            <form className="flex flex-col text-start gap-5">
+                <div className="flex flex-col gap-2">
+                    <label>Enter the name</label>
+                    <div className="h-10 relative">
+                        <input type="text" value={formData.name} onChange={(e) => setFormData({...formData, name:e.target.value})}/>
+                        <FaRegUser className="absolute top-1/2 -translate-y-1/2 right-3"/>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label>Enter the password</label>
+                    <div className="h-10 relative">
+                        <input  type="password" value={formData.password} onChange={(e) => setFormData({...formData, password:e.target.value})}/>
+                        <RxLockClosed className="absolute top-1/2 -translate-y-1/2 right-3"/>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <label>Repeat the password</label>
+                    <div className="h-10 relative">
+                        <input className="border-2 border-blue-700" type="password" value={formData.password} onChange={(e) => setFormData({...formData, password:e.target.value})}/>
+                        <RxLockClosed className="absolute top-1/2 -translate-y-1/2 right-3"/>
+                    </div>
+                </div>
+                <button type="submit" onClick={handleSubmit}>Login</button>
+            </form>
+            <button onClick={handelExit}>Exit</button>
+        </div>
     </div>
 }
