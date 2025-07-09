fee2813e0e6bfbe04bbab9c1bf6b5095f1f2d659
melkiurak <melkiurak3209@gmail.com>
2025-07-09 19:05:50 +0300

    Add image blokc

--- src/page/Profile/SendPost/SendPost.tsx
+++ src/page/Profile/SendPost/SendPost.tsx
@@ -1,7 +1,7 @@
 import { BsCardImage } from "react-icons/bs";
 import { IoMdColorFilter } from "react-icons/io";
 import { FaCode } from "react-icons/fa";
-import { FaAlignLeft, FaLink  } from "react-icons/fa6";
+import { FaAlignLeft, FaLink, FaPlus   } from "react-icons/fa6";
 import React from "react";
 export const SendPost = () => {
     const postTools = [
@@ -12,28 +12,51 @@ export const SendPost = () => {
         {name: 'Link', icon: FaLink},
     ]
     return <div>
-        <form className="flex flex-col">
-            <div>
+        <form className="flex flex-col gap-5">
+            <div className="flex flex-col gap-[15px]">
                 <label htmlFor=""  className="text-h5">Title</label>
-                <input type="text" />
+                <div className="h-12">
+                    <input type="text" className="h-full" />
+                </div>
             </div>
-            <div>
+            <div className="flex flex-col gap-[15px]">
                 <label htmlFor=""  className="text-h5">Add Tags</label>
-                <input type="text" />
-                <button>+</button>
+                <div className="h-12 relative">
+                    <input type="text" className="h-full" />
+                    <button className="bg-[#0000000C] text-[#3E3232] p-2 rounded-xl absolute right-1 top-1/2 -translate-y-1/2"><FaPlus className="text-2xl"/></button>
+                </div>
             </div>
-            <div>
+            <div className="flex flex-col gap-[15px]">
                 <label htmlFor="" className="text-h5">Explanation</label>
-                <div>
-                    <div>
+                <div className="bg-white rounded-xl shadow-xs px-3 py-5">
+                    <div className="flex justify-between items-center">
                         {postTools.map((tools, index) => (
-                            <button key={index}>
-                                <span>{React.createElement(tools.icon)}</span>
-                                <span>{tools.name}</span>
+                            <button key={index} className="bg-[#F5F5F5] rounded-xl px-4 py-2.5">
+                                <span className="text-[#3E323280] text-base">{React.createElement(tools.icon)}</span>
+                                <span className="hidden">{tools.name}</span>
                             </button>
                         ))}
                     </div>
-                    <input type="text" />
+                    <textarea
+                        id="Explanation"
+                        name="Explanation"
+                        placeholder="Type..."
+                        rows="4"
+                        className="border-none bg-[#F5F5F5] p-5 rounded-xl w-full h-[377px] resize-none outline-none mt-6"
+                    />                 
+                </div>
+            </div>
+            <div className="flex flex-col gap-[15px]">
+                <label htmlFor="">Add Image</label>
+                <div className="bg-[#F5F5F5] p-2.5 rounded-xl">
+                    <div className="flex flex-col items-center gap-8 justify-center border-2 border-dashed border-[#E1E1E1] rounded-xl h-[360px]">
+                        <BsCardImage className="w-[120px] h-[96px] text-[#3E323240] "/>
+                        <p className="text-Input text-[#3E3232BF]">Drop image here, paste or</p>
+                        <button className="flex items-center justify-center py-1 px-4 gap-2 text-[#3E323280] border-2 border-[#E6E6E6] rounded-xl">
+                            <FaPlus/>
+                            <span className="text-btn">Select</span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </form>
