310c2c18081c70c7c71d890146c7c5465497d581
melkiurak <melkiurak3209@gmail.com>
2025-07-10 14:31:43 +0300

    Finally end the adaptive for senpost

--- src/page/Profile/SendPost/SendPost.tsx
+++ src/page/Profile/SendPost/SendPost.tsx
@@ -12,67 +12,73 @@ export const SendPost = () => {
         {name: 'Link', icon: FaLink},
     ]
     return <div>
-        <form className="flex flex-col gap-5">
-            <div className="flex flex-col gap-[15px]">
-                <label htmlFor=""  className="text-h5">Title</label>
-                <div className="h-12">
-                    <input type="text" className="h-full" />
-                </div>
-            </div>
-            <div className="flex flex-col gap-[15px]">
-                <label htmlFor=""  className="text-h5">Add Tags</label>
-                <div className="h-12 relative">
-                    <input type="text" className="h-full" />
-                    <button className="bg-[#0000000C] text-[#3E3232] p-2 rounded-xl absolute right-1 top-1/2 -translate-y-1/2"><FaPlus className="text-2xl"/></button>
-                </div>
-            </div>
-            <div className="flex flex-col gap-[15px]">
-                <label htmlFor="" className="text-h5">Explanation</label>
-                <div className="bg-white rounded-xl shadow-xs px-3 py-5">
-                    <div className="flex justify-between items-center">
-                        {postTools.map((tools, index) => (
-                            <button key={index} className="bg-[#F5F5F5] rounded-xl px-4 py-2.5">
-                                <span className="text-[#3E323280] text-base">{React.createElement(tools.icon)}</span>
-                                <span className="hidden">{tools.name}</span>
-                            </button>
-                        ))}
+        <form className="flex flex-col lg:flex-row gap-5">
+            <div className="flex flex-col gap-6 flex-1">
+                <div className="flex gap-6 flex-col lg:flex-row justify-between">
+                    <div className="flex flex-col gap-[15px] flex-1">
+                        <label htmlFor=""  className="text-h5">Title</label>
+                        <div className="h-12">
+                            <input type="text" className="h-full" />
+                        </div>
                     </div>
-                    <textarea
-                        id="Explanation"
-                        name="Explanation"
-                        placeholder="Type..."
-                        rows="4"
-                        className="border-none bg-[#F5F5F5] p-5 rounded-xl w-full h-[377px] resize-none outline-none mt-6"
-                    />                 
+                    <div className="flex flex-col gap-[15px] flex-1">
+                        <label htmlFor=""  className="text-h5">Add Tags</label>
+                        <div className="h-12 relative">
+                            <input type="text" className="h-full" />
+                            <button className="bg-[#0000000C] text-[#3E3232] p-2 rounded-xl absolute right-1 top-1/2 -translate-y-1/2"><FaPlus className="text-2xl"/></button>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-[15px]">
+                    <label htmlFor="" className="text-h5">Explanation</label>
+                    <div className="bg-white rounded-xl shadow-[0_0_20px_rgba(0,0,0,0.1)] px-3 py-5">
+                        <div className="flex justify-between items-center">
+                            {postTools.map((tools, index) => (
+                                <button key={index} className="bg-[#F5F5F5] rounded-xl px-4 py-2.5">
+                                    <span className="text-[#3E323280] text-base">{React.createElement(tools.icon)}</span>
+                                    <span className="hidden">{tools.name}</span>
+                                </button>
+                            ))}
+                        </div>
+                        <textarea
+                            id="Explanation"
+                            name="Explanation"
+                            placeholder="Type..."
+                            rows="4"
+                            className="border-none bg-[#F5F5F5] p-5 rounded-xl w-full h-[377px] resize-none outline-none mt-6"
+                        />                 
+                    </div>
                 </div>
             </div>
-            <div className="flex flex-col gap-[15px]">
-                <label htmlFor="">Add Image</label>
-                <div className="bg-[#F5F5F5] p-2.5 rounded-xl">
-                    <div className="flex flex-col items-center gap-8 justify-center border-2 border-dashed border-[#E1E1E1] rounded-xl h-[360px]">
-                        <BsCardImage className="w-[120px] h-[96px] text-[#3E323240] "/>
-                        <p className="text-Input text-[#3E3232BF]">Drop image here, paste or</p>
-                        <button className="flex items-center justify-center py-1 px-4 gap-2 text-[#3E323280] border-2 border-[#E6E6E6] rounded-xl">
-                            <FaPlus/>
-                            <span className="text-btn">Select</span>
+            <div className="flex flex-col gap-[25px] lg:max-w-[360px] w-full">
+                <div className="flex flex-col gap-[15px]">
+                    <label htmlFor="">Add Image</label>
+                    <div className="bg-[#F5F5F5] p-2.5 rounded-xl">
+                        <div className="flex flex-col items-center gap-8 justify-center border-2 border-dashed border-[#E1E1E1] rounded-xl h-[360px] md:h-[394px]">
+                            <BsCardImage className="w-[120px] h-[96px] text-[#3E323240] "/>
+                            <p className="text-Input text-[#3E3232BF]">Drop image here, paste or</p>
+                            <button className="flex items-center justify-center py-1 px-4 gap-2 text-[#3E323280] border-2 border-[#E6E6E6] rounded-xl">
+                                <FaPlus/>
+                                <span className="text-btn">Select</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-end">
+                    <div className="flex justify-between gap-3 max-w-[280px] md:max-w-[360px] w-full">
+                        <button className="flex-1 bg-[#F5F5F5] rounded-xl  flex items-center justify-center py-2 gap-2 text-[#3E3232BF]">
+                            <FaRegFloppyDisk/>
+                            <span className="text-btn">Draft</span>
+                        </button>
+                        <button className="flex-1 bg-[#F5F5F5] rounded-xl  flex items-center justify-center py-2 gap-2 text-[#3E3232BF]">
+                            <FaRegEye/>
+                            <span className="text-btn">Preview</span>
+                        </button>
+                        <button className="flex-1 bg-[#F81539BF] rounded-xl flex items-center justify-center py-2 gap-2 text-white">
+                            <IoMdPaperPlane/>
+                            <span className="text-btn">Public</span>
                         </button>
                     </div>
-                </div>
-            </div>
-            <div className="flex justify-end">
-                <div className="flex justify-between gap-3 max-w-[280px] md:max-w-[360px] w-full">
-                    <button className="flex-1 bg-[#F5F5F5] rounded-xl  flex items-center justify-center py-2 gap-2 text-[#3E3232BF]">
-                        <FaRegFloppyDisk/>
-                        <span className="text-btn">Draft</span>
-                    </button>
-                    <button className="flex-1 bg-[#F5F5F5] rounded-xl  flex items-center justify-center py-2 gap-2 text-[#3E3232BF]">
-                        <FaRegEye/>
-                        <span className="text-btn">Preview</span>
-                    </button>
-                    <button className="flex-1 bg-[#F81539BF] rounded-xl flex items-center justify-center py-2 gap-2 text-white">
-                        <IoMdPaperPlane/>
-                        <span className="text-btn">Public</span>
-                    </button>
                 </div>
             </div>
         </form>
