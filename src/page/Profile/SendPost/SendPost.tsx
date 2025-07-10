35a30b7278b8017d112a46e6b8400bb9473c375d
melkiurak <melkiurak3209@gmail.com>
2025-07-10 14:39:32 +0300

    ADD

--- src/page/Profile/SendPost/SendPost.tsx
+++ src/page/Profile/SendPost/SendPost.tsx
@@ -18,13 +18,13 @@ export const SendPost = () => {
                     <div className="flex flex-col gap-[15px] flex-1">
                         <label htmlFor=""  className="text-h5">Title</label>
                         <div className="h-12">
-                            <input type="text" className="h-full" />
+                            <input type="text" className="h-full pl-4" />
                         </div>
                     </div>
                     <div className="flex flex-col gap-[15px] flex-1">
                         <label htmlFor=""  className="text-h5">Add Tags</label>
                         <div className="h-12 relative">
-                            <input type="text" className="h-full" />
+                            <input type="text" className="h-full pl-4" />
                             <button className="bg-[#0000000C] text-[#3E3232] p-2 rounded-xl absolute right-1 top-1/2 -translate-y-1/2"><FaPlus className="text-2xl"/></button>
                         </div>
                     </div>
@@ -32,11 +32,11 @@ export const SendPost = () => {
                 <div className="flex flex-col gap-[15px]">
                     <label htmlFor="" className="text-h5">Explanation</label>
                     <div className="bg-white rounded-xl shadow-[0_0_20px_rgba(0,0,0,0.1)] px-3 py-5">
-                        <div className="flex justify-between items-center">
+                        <div className="flex justify-between items-center lg:max-w-[500px] gap-5">
                             {postTools.map((tools, index) => (
-                                <button key={index} className="bg-[#F5F5F5] rounded-xl px-4 py-2.5">
+                                <button key={index} className="bg-[#F5F5F5] rounded-xl px-4 py-2.5 flex items-center gap-2 flex-1">
                                     <span className="text-[#3E323280] text-base">{React.createElement(tools.icon)}</span>
-                                    <span className="hidden">{tools.name}</span>
+                                    <span className="hidden md:block text-btn text-[#3E32324B]">{tools.name}</span>
                                 </button>
                             ))}
                         </div>
@@ -74,7 +74,7 @@ export const SendPost = () => {
                             <FaRegEye/>
                             <span className="text-btn">Preview</span>
                         </button>
-                        <button className="flex-1 bg-[#F81539BF] rounded-xl flex items-center justify-center py-2 gap-2 text-white">
+                        <button type="submit" className="flex-1 bg-[#F81539BF] rounded-xl flex items-center justify-center py-2 gap-2 text-white">
                             <IoMdPaperPlane/>
                             <span className="text-btn">Public</span>
                         </button>
