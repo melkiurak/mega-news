0ab275049c55fd8fb463cb843824c053e60fdd6d
melkiurak <melkiurak3209@gmail.com>
2025-07-14 19:13:55 +0300

    As

--- src/components/TagsView/TagsView.tsx
+++ src/components/TagsView/TagsView.tsx
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react"
 import { dataTags } from "../../server/getData"
 import type { Tags } from "../../types";
+import { ButtonSwitch } from "../buttonSwitch/ButtonSwitch";
 
 export const TagsView = () => {
     const [dataTag, setDataTag] = useState<Tags[]>([]);
+    const [slide, setSilde] = useState(0);
+    const visibleCount = 7.6;
+    const handleNexthBtn = () => {
+        setSilde(s => Math.max(s - 1, 0))
+    }; 
+      const handlePrevBtn = () => {
+        setSilde(s => Math.min(s + 1, dataTag.length - visibleCount))
+    } 
     useEffect(() => {
         const fecthTags = async () => {
             const result = await dataTags();
@@ -11,12 +20,15 @@ export const TagsView = () => {
         };
         fecthTags()
     }, []);
-    return <div className="w-full h-12">
-        <div className="flex items-center justify-between gap-6 w-full h-full flex-wrap overflow-hidden">{dataTag.map((tag, index) => (
-            <div key={index} className="max-w-[170px] w-full h-full rounded-xl relative overflow-hidden">
+    return <div className="w-full h-[68px]">
+        <div className="relative flex items-center justify-between  w-full h-full overflow-hidden bg-[#F5F5F5] p-2.5 rounded-xl">
+            {dataTag.slice(slide,slide + visibleCount).map((tag, index) => (
+            <div key={index} className="max-w-[170px] w-full h-12 rounded-xl relative overflow-hidden">
                 <div className="blur-[2px] bg-no-repeat bg-cover bg-center w-full h-full absolute top-0 left-0 rounded-xl"  style={{backgroundImage: `url(${tag?.image})`, boxShadow: 'inset 0 0 40px rgba(0, 0, 0, 0.8)',}}></div>
                 <p className="text-h5 text-white absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">#{tag.name}</p>
             </div>
-        ))}</div>
+            ))}
+            <ButtonSwitch prev={handlePrevBtn} next={handleNexthBtn} />
+        </div>
     </div>
 }
--- src/components/buttonSwitch/ButtonSwitch.tsx
+++ src/components/buttonSwitch/ButtonSwitch.tsx
@@ -1,8 +1,13 @@
 import { IoIosArrowBack, IoIosArrowForward  } from "react-icons/io";
 
-export const ButtonSwitch = () => {
-    return <div className="flex justify-between items-center w-full">
-        <button className='bg-[#F5F5F5] rounded-xl w-10 h-10 flex items-center justify-center'><IoIosArrowBack className="text-[#3E3232] text-2xl"/></button>
-        <button className='bg-[#F5F5F5] rounded-xl w-10 h-10 flex items-center justify-center'><IoIosArrowForward className="text-[#3E3232] text-2xl"/></button>
+interface Button {
+    next: () => void ,
+    prev: () => void,
+}
+
+export const ButtonSwitch = ({next, prev}: Button) => {
+    return <div className="flex justify-between items-center w-full absolute">
+        <button className='bg-[#F5F5F5] rounded-xl w-10 h-10 flex items-center justify-center' onClick={next}><IoIosArrowBack className="text-[#3E3232] text-2xl"/></button>
+        <button className='bg-[#F5F5F5] rounded-xl w-10 h-10 flex items-center justify-center' onClick={prev}><IoIosArrowForward className="text-[#3E3232] text-2xl"/></button>
     </div>
 }
