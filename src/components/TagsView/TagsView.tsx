d86897646d456660f1bdc9550a1f5a76073b9af4
melkiurak <melkiurak3209@gmail.com>
2025-07-14 18:54:07 +0300

    Add to style for tags

--- src/components/TagsView/TagsView.tsx
+++ src/components/TagsView/TagsView.tsx
@@ -13,8 +13,9 @@ export const TagsView = () => {
     }, []);
     return <div className="w-full h-12">
         <div className="flex items-center justify-between gap-6 w-full h-full flex-wrap overflow-hidden">{dataTag.map((tag, index) => (
-            <div key={index} className="blur-sm bg-no-repeat bg-cover bg-center max-w-[170px] w-full h-full rounded-xl" style={{backgroundImage: `url(${tag?.image})`}}>
-                <p className="text-h5 text-white">#{tag.name}</p>
+            <div key={index} className="max-w-[170px] w-full h-full rounded-xl relative overflow-hidden">
+                <div className="blur-[2px] bg-no-repeat bg-cover bg-center w-full h-full absolute top-0 left-0 rounded-xl"  style={{backgroundImage: `url(${tag?.image})`, boxShadow: 'inset 0 0 40px rgba(0, 0, 0, 0.8)',}}></div>
+                <p className="text-h5 text-white absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">#{tag.name}</p>
             </div>
         ))}</div>
     </div>
