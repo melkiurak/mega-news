b8e597a75bfc5ee2cc11f6f820504b665fc0366f
melkiurak <melkiurak3209@gmail.com>
2025-07-17 14:08:38 +0300

    Add loading

---
+++ src/components/loading/loading.tsx
@@ -0,0 +1,5 @@
+export const Loading = () => {
+    return <div>
+        <h2 className="text-[50px] font-roboto font-bold text-[#FC4308]">MEGA.news</h2>
+    </div>
+}
--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -3,9 +3,24 @@ import { PopularPost } from '@components/Posts/PopularPost/PopularPost'
 import { NewPost } from '@components/Posts/NewPost/NewPost'
 import { TrendyPosts } from '@components/Posts/TrendyPost/TrendyPosts'
 import { TopPost } from '@components/Posts/TopPost/TopPost'
+import { useEffect, useState } from 'react'
+import { Loading } from '@components/loading/loading'
 
 export  const Home = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+           setLoading(false) 
+        }, 3000);
+        return () => clearTimeout(timer);
+    }, [])
+
+  if (loading) {
+    return <Loading />;
+  }
     return <div className='w-full flex flex-col gap-10 phone:gap-[50px] lg:gap-[70px]'>
+        {loading && <Loading/>}
         <TagsView/>
         <PopularPost />
         <NewPost/>
